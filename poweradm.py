a226d36ed7f0984bbc7edbb860a3b13a31600cf6
Kairo Araujo <kairo@kairo.eti.br>
2014-11-25 12:27:52 -0200

    Working

--- poweradm.py
+++ poweradm.py
@@ -17,13 +17,23 @@ systems = ['P1-8205-E6D-SN06A07AT', 'P1-8205-E6D-SN06A07BT', 'P1-8205-E6D-SN06A0
 # Virtual Switches
 virtual_switches = ['VSW-GERENCIA-01', 'VSW-DADOS-01', 'VSW-BACKUP-01']
 #
+##############################################################################################
+#     !! CAUTION !!  Only modify below if you are sure what you are doing !! CAUTION !!      #
+##############################################################################################
+#
+# Importing classes/modules
 import time
 import os.path
+##############################################################################################
 #
+# Global Variables
 timestr = time.strftime("%d%m%Y-%H%M%S")
+version = '0.1b'
 #
+# Classes
+##############################################################################################
 
-
+# get a next free id on systems
 class newId:
     def __init__(self, ids, newid):
         self.ids = ids
@@ -51,16 +61,26 @@ class newId:
         return self.newid
 
 
-
-
 # Main
 
+os.system('clear')
+print ("\n\n[ Power Adm ]\n[ Version: %s - © 2014 Kairo Araujo - BSD License ]\n\n" % version)
+
+print ("[LPAR Configuration ]\n")
+change = input("Change or Ticket number: ")
 
 freeid = newId('nids', 'nnewid')
 freeid.mkId()
 freeid.getId()
 
-
+print ("\nSelect the system host for LPAR")
+systems_length = (len(systems))-1
+count = 0
+while count <= systems_length:
+    print ("%s : %s" % (count, systems[count]))
+    count += 1
+system_option = int(input("System: "))
+#print ("%s" % systems[system_option]) - test
 
 
 
