cc37c00bffc233bd2719dc129995d37e71ff0c2f
Kairo Araujo <kairo@kairo.eti.br>
2014-11-27 15:55:08 -0200

    in developing

--- globalvar.py
+++ globalvar.py
@@ -1,11 +1,33 @@
 #!/usr/bin/env python
-## -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 #
-##############################################################################################
-#     !! CAUTION !!  Only modify below if you are sure what you are doing !! CAUTION !!      #
-##############################################################################################
+# Power Adm
+# globalvar.py
 #
-# Importing classes/modules
+# Copyright (c) 2014 Kairo Araujo
+#
+# This program was created to help in managing vhost devices and its hdisks
+# in PowerVM (vios) servers. It was created for personal use. There are no
+# guarantees of the author. Use at your own risk.
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+# Important:
+# IBM, PowerVM (a.k.a. vios) are registered trademarks of IBM Corporation in
+# the United States, other countries, or both.
+#
+# Imports
+###############################################################################################
 import time
 import os.path
 from config import *
